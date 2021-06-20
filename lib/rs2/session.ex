ccc49f91e8893efdbf822cc053695b95c3247eac
Lester Burnham <85967301+lesterburnham@users.noreply.github.com>
2021-06-20 15:36:10 +0000

    Change to start.

--- lib/rs2/session.ex
+++ lib/rs2/session.ex
@@ -1,6 +1,6 @@
 defmodule RS2.Session do
   def send_packet(session, packet) do
-    Task.async(fn ->
+    Task.start(fn ->
       case Registry.lookup(RS2.Xyz, session) do
         [{pid, _}] ->
           GenServer.call(pid, {:send_packet, packet})
